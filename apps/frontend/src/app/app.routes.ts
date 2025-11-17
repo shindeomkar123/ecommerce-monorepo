c3bdd2b2acb11d5ce19af71f29e1679b7beefd99
Omkar Shinde <33689953+shindeomkar123@users.noreply.github.com>
2025-11-17 18:59:44 +0530

    ui changes

--- apps/frontend/src/app/app.routes.ts
+++ apps/frontend/src/app/app.routes.ts
@@ -3,11 +3,16 @@ import { Login } from './login/login';
 import { Home } from './home/home';
 import { Cart } from './cart/cart';
 import { Summary } from './summary/summary';
+import { Details } from './details/details';
 
 export const appRoutes: Route[] = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: Login },
-  { path: 'catalog', component: Home },
+  {
+    path: 'catalog',
+    component: Home,
+  },
+  { path: 'catalog/:id', component: Details },
   { path: 'cart', component: Cart },
   { path: 'summary', component: Summary },
 ];
--- apps/frontend/src/app/login/login.ts
+++ apps/frontend/src/app/login/login.ts
@@ -1,9 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Button } from '@ecommerce-monorepo/ui';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [Button, ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
-export class Login {}
+export class Login {
+  fb = inject(FormBuilder);
+
+  myForm = this.fb.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+
+  onSubmit() {
+    if (this.myForm.valid) {
+      const email = this.myForm.get('email');
+      const password = this.myForm.get('password');
+      console.log(email, password);
+    }
+  }
+}
